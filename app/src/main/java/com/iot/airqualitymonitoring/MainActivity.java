70a65a123c1509a070df744d616c0b741282840e
OguzKaanYazan <oguz.yazan@isik.edu.tr>
2021-01-27 20:05:12 +0300

    main activity updated

--- app/src/main/java/com/iot/airqualitymonitoring/MainActivity.java
+++ app/src/main/java/com/iot/airqualitymonitoring/MainActivity.java
@@ -1,7 +1,14 @@
 package com.iot.airqualitymonitoring;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +25,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -25,6 +33,10 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static int REQUEST_ENABLE_BT = 1;
+    private static UUID MY_UUID = UUID.fromString("7ff8a1fe-23fd-4f7b-84be-33d822e5868d");
+    private static String TAG = "FragmentActivity";
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private ArrayList<Measurement> measurements;
     private ListView listView;
     private MeasurementListAdapter listViewAdapter;
@@ -34,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enableBluetooth();
+        //bluetoothAdapter.startDiscovery();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(receiver, filter);
         initialize();
         fillArrayList(measurements);
     }
@@ -53,14 +73,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private String serviceRequest(String url) throws IOException{
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+    private void enableBluetooth(){
+        if (bluetoothAdapter != null) {
+            // Device doesn't support Bluetooth
 
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Toast.makeText(getApplicationContext(),"Discovery Start",Toast.LENGTH_SHORT).show();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Toast.makeText(getApplicationContext(),"Discovery End",Toast.LENGTH_SHORT).show();
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //bluetooth device found
+                BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                Toast.makeText(getApplicationContext(),"Found Device " + device.getName(),Toast.LENGTH_SHORT).show();
+            } else if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                if(state == BluetoothAdapter.STATE_ON){
+                    bluetoothAdapter.startDiscovery();
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
 }
 
