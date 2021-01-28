53821229306c030772473af9be23ea81ba1685b7
VolkanDavsan <57091151+VolkanDavsan@users.noreply.github.com>
2021-01-28 23:38:06 +0300

    Arduino code added.

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,19 @@
+int input = A0;
+#include <SoftwareSerial.h>
+SoftwareSerial bt_conn(7,6); //RX TX
+
+
+void setup() {
+ 
+ Serial.begin(9600);
+ bt_conn.begin(9600);
+
+}
+
+void loop() {
+  bt_conn.println(analogRead(input));
+  Serial.println(analogRead(input));
+
+  delay(60000);
+
+}
