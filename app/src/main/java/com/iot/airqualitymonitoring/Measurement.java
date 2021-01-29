178a6c9c4f2d3de630edb7de360c58ac5d410d7f
VolkanDavsan <57091151+VolkanDavsan@users.noreply.github.com>
2021-01-29 20:48:46 +0300

    Measurement variables updated

--- app/src/main/java/com/iot/airqualitymonitoring/Measurement.java
+++ app/src/main/java/com/iot/airqualitymonitoring/Measurement.java
@@ -24,24 +24,42 @@ public class Measurement {
     public void setDate(String date) {
         this.date = date;
     }
-
     public int getStatusImg() {
-        if (this.value < 100) {
+        if (this.value < 250) {
             return R.drawable.great; //GREAT
-        } else if (this.value >= 100 && this.value < 200) {
-            return R.drawable.okay; //OKAY
-        } else {
-            return R.drawable.unhealthy; //BAD
+        } else if (this.value >= 250 && this.value<350){
+            return  R.drawable.okay;
+        }
+        else if (this.value >= 350 && this.value < 400) {
+            return R.drawable.beware; //OKAY
+        }
+        else if (this.value>=400 && this.value<500){
+            return R.drawable.unhealthy;
+        }
+        else if(this.value>=500 && this.value<650){
+            return R.drawable.vunhealthy;
+        }
+        else {
+            return R.drawable.hazardous; //BAD
         }
     }
 
     public String getStatusTxt() {
-        if (this.value < 100) {
+        if (this.value < 250) {
             return "Great!";
-        } else if (this.value >= 100 && this.value < 200) {
+        } else if (this.value >= 250 && this.value < 350) {
             return "Okay.";
-        } else {
-            return "Unhealthy!";
+        } else if (this.value>=350&&this.value<400){
+            return "Sensitive beware.";
+        }
+        else if (this.value>=400 && this.value<500){
+            return "Unhealthy";
+        }
+        else if (this.value>=500 && this.value<650){
+            return "Very Unhealthy.";
+        }
+            else {
+            return "HAZARDOUS";
         }
     }
 }
